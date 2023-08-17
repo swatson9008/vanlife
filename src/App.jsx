a5d72a32fb5df2cd55a043889dfc722f7affa57b
Summer <swatson9008@yahoo.com>
2023-08-17 12:08:03 -0400

    installing dependcies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import { ReactDOM } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
+    <h1>Hello World</h1>
+    
+    </BrowserRouter>
 
-    </>
   )
 }
 
