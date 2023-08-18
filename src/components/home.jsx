8d1bed8a9a4485ccc5a5f1e69af4be2db37b5760
Summer <swatson9008@yahoo.com>
2023-08-18 17:30:47 -0400

    working on home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,20 @@
-import Mountain from '../assets/mountain.jpeg'
+import './home.css'
 
 export default function Home() {
     return (
       <div className='homeMain'>
-        <img src={Mountain} alt="" className='mountainSplash'/>
+        <div className='overlayColor'>
+          <div className='homeInfo'> 
+            <span className='lineOne'>
+              You got the travel plans, we got the travel vans.
+            </span>
+            <span className='lineTwo'>
+              Add adventure to your life by joining the #vanlife movement.<br />
+              Rent the perfect van to make your perfect road trip.
+            </span>
+            <button className='vanButton'>Find your van</button>
+          </div>
+        </div>
       </div>
     )
   }
