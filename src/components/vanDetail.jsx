91f6c43d87b4d3fc713300fa6da3394bb632d757
Summer <swatson9008@yahoo.com>
2023-08-22 15:20:13 -0400

    made a detailed vans page

--- src/components/vanDetail.jsx
+++ src/components/vanDetail.jsx
@@ -1,3 +1,30 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import './vanDetail.css'
+
 export default function VanDetail() {
-    return <h1>Van detail page goes here</h1>
+  const params = useParams();
+  const [vanData, setVanData] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((response) => response.json())
+      .then((data) => setVanData(data.vans))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, [params.id]);
+
+  if (!vanData) {
+    return <p className="loadingText">Loading your vans!</p>;
+  }
+
+  return (
+    <div className="vanContainer">
+      <h2>{vanData.name}</h2>
+      <img src={vanData.imageUrl} alt=""/>
+      <p><button className={`vanType ${vanData.type}`}>{vanData.type}</button></p>
+      <p className="vanPrice">{`$${vanData.price}/day`}</p>
+      <p>{vanData.description}</p><br />
+      <button className="rentVan">Rent this van</button>
+    </div>
+  );
 }
