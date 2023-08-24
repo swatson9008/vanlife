4326d5ed026c1782f92d7399beab14004b00b0c3
Summer <swatson9008@yahoo.com>
2023-08-24 12:49:09 -0400

    code clean up

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from './components/home';
 import About from './components/about';
-import Navbar from './components/navbar';
 import Vans from './components/vans'
 import VanDetail from './components/vanDetail';
 import Layout from './components/layout';
