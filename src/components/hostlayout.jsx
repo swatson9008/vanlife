27f6c9564d3c19f39c1dd530611fca146eb82ac7
Summer <swatson9008@yahoo.com>
2023-08-25 17:54:31 -0400

    css fixes

--- src/components/hostlayout.jsx
+++ src/components/hostlayout.jsx
@@ -5,11 +5,9 @@ export default function HostLayout(){
     return(
         <>
         <main className="hostMain">
-            <div className="navLinks">
                 <Link to="/host">Dashboard</Link>
                 <Link to="/host/income">Income</Link>
                 <Link to="/host/reviews">Reviews</Link>
-            </div>
             
         </main>
         <Outlet />
