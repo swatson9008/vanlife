4901c965a39c60be2eaf528281a21a639bc241d6
Summer <swatson9008@yahoo.com>
2023-08-28 16:03:27 -0400

    navbar styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,23 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import './navbar.css'
 
 export default function Navbar(){
+  const activeStyle = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616"
+  }
 
     return(
       <div className="NavBar">
         <span><Link to="/">#VANLIFE</Link></span>
         <div className="navLinks">
-          <Link to="/host">Host</Link>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
+          <NavLink to="/host"
+          style={({isActive}) => isActive ? activeStyle : null }>Host</NavLink>
+          <NavLink to="/about"
+          style={({isActive}) => isActive ? activeStyle : null }>About</NavLink>
+          <NavLink to="/vans"
+          style={({isActive}) => isActive ? activeStyle : null }>Vans</NavLink>
         </div>
       </div>
     )
