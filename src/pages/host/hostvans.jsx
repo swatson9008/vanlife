d95c529710d5fea3dc23f9e10cd8f87e0e6917ed
Summer <swatson9008@yahoo.com>
2023-09-06 20:36:22 -0400

    added apis

--- src/pages/host/hostvans.jsx
+++ src/pages/host/hostvans.jsx
@@ -2,38 +2,57 @@
 /* eslint-disable react/jsx-no-undef */
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import './hostvans.css'
+import { getHostVans } from "../../../api";
+import "./hostvans.css";
 
 export default function Vans() {
   const [vanData, setVanData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("/api/host/vans")
-      .then((response) => response.json())
-      .then((data) => setVanData(data.vans))
-      .catch((error) => console.error("Error fetching data:", error));
+    async function loadVans() {
+      setLoading(true);
+      try {
+        const data = await getHostVans();
+        setVanData(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadVans();
   }, []);
-  
+
+  if (error) {
+    return <h1>There was an error: {error.message}</h1>;
+  }
+
   return (
     <>
-    <main className="vanHostMain">
+      <main className="vanHostMain">
         <h2>Your Listed Vans</h2>
-      <div className="vanHostData">
-        {vanData ? (
-          vanData.map((van) => (
-            <Link to={van.id} key={van.id}>
-              <div key={van.id} className="vanHostItem">
-              <div className="vanHostImg"><img src={van.imageUrl} alt=""/></div>
-              <div className="vanHostInfo"><div className="vanHostName">{van.name}</div>
-              <div className="vanHostPrice">${van.price}/day</div></div>
-            </div></Link>
-          ))
-        ) : (
-          <p>Loading your vans!</p>
-        )}
-      </div>
-    </main>
+        <div className="vanHostData">
+          {vanData ? (
+            vanData.map((van) => (
+              <Link to={van.id} key={van.id}>
+                <div key={van.id} className="vanHostItem">
+                  <div className="vanHostImg">
+                    <img src={van.imageUrl} alt="" />
+                  </div>
+                  <div className="vanHostInfo">
+                    <div className="vanHostName">{van.name}</div>
+                    <div className="vanHostPrice">${van.price}/day</div>
+                  </div>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <p>Loading your vans!</p>
+          )}
+        </div>
+      </main>
     </>
   );
 }
-
