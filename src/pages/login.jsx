699006ddb55276e02c4174a06e10980850e704cb
Summer <swatson9008@yahoo.com>
2023-09-08 13:12:53 -0400

    editing the log in page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -8,13 +8,24 @@ export default function Login() {
     email: "",
     password: "",
   });
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
   const location = useLocation();
   function handleSubmit(e) {
     e.preventDefault();
+    setStatus("submitting");
     loginUser(loginFormData)
-    .then((result) => {
-      console.log(result);
-    })
+      .then((data) => {
+        console.log(data);
+        setError(null);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error);
+      })
+      .finally(() => {
+        setStatus("idle");
+      });
   }
 
   function handleChange(e) {
@@ -27,8 +38,11 @@ export default function Login() {
 
   return (
     <div className="loginMain">
-      {location.state?.message && <h3 className="logInFirst">{location.state.message}</h3>}
+      {location.state?.message && (
+        <h3 className="logInFirst">{location.state.message}</h3>
+      )}
       <h1>Sign in to your account</h1>
+      {error && <h3 className="errorState">{error}</h3>}
       <form onSubmit={handleSubmit} className="loginForm">
         <input
           name="email"
@@ -44,7 +58,8 @@ export default function Login() {
           placeholder="Password"
           value={loginFormData.password}
         />
-        <button>Log in</button>
+        <button disabled={status === "submitting"}>
+          {status === "submitting" ? "Logging in..." : "Log in"}</button>
       </form>
     </div>
   );
