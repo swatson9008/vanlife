4558c7a85e0e9a726955cef0c11ad5a1badc4e03
Summer <swatson9008@yahoo.com>
2023-09-08 16:35:49 -0400

    added replace prop

--- src/components/AuthRequired.jsx
+++ src/components/AuthRequired.jsx
@@ -1,11 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function AuthRequired() {
-  const isLoggedIn = localStorage.getItem("loggedin")
+  const isLoggedIn = localStorage.getItem("loggedin");
 
   if (!isLoggedIn) {
     return (
-      <Navigate to="/login" state={{ message: "You must log in first" }} />
+      <Navigate to="/login" state={{ message: "You must log in first" }} replace />
     );
   }
   return <Outlet />;
